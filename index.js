3f47b776330bd2a32c48a7a280c2099a97f1f54d
Adesh <Adeshgangwar1996@gmail.com>
2021-01-16 21:01:20 +0530

    APis test working

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("log working...");
+const express = require("express");
+const app = express();
+const port = 9096;
+
+app.get("/", async (req, res) => {
+  res.send({
+    ok: true,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App is listening at http://localhost:${port}`);
+});
